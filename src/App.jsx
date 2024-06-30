142fda16101123b2595ddba8eaf35a0213cff1c9
mhdabdellah <mhdabellahi0@gmail.com>
2024-06-30 12:44:07 +0000

    props,propsTypes,and defaultProps

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@
 // import Footer from "./footer"
 // import Food from "./food"
 // import Card from "./app_components/card_component/card"
-import AppButton from "./app_components/button_component/button"
+// import AppButton from "./app_components/button_component/button"
+import Student from "./app_components/student_component/student"
 
 function App() {
   return(
@@ -13,10 +14,15 @@ function App() {
     //   <Card/>
     //   <Footer/>
     // </>
+    // <>
+    //   <AppButton/>
+    // </>
     <>
-      <AppButton/>
+      <Student name="Mohamed Abdellahi Sidi Mohamed Blal" age={25} isStudent={true} />
+      <Student name="Mohamed Sidi Mohamed Blal" age={20} isStudent={false} />
+      <Student name="Mohamed Sidi Mohamed Blal" age={20} isStudent={true} />
+      <Student />
     </>
-    
 
 
   )
---
+++ src/app_components/student_component/student.jsx
@@ -0,0 +1,31 @@
+import style from "./student.module.css"
+import PropTypes from "prop-types"
+
+function Student(props){
+    return(
+        <div className={style.student}>
+            <p>
+                Name : {props.name}
+            </p>
+            <p>
+                Age : {props.age}
+            </p>
+            <p>
+                Is Student : {props.isStudent ? "Yes" : "No"}
+            </p>
+        </div>
+    );
+}
+
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+}
+Student.defaultProps = {
+    name: "Mauritannien",
+    age: 0,
+    isStudent: false
+}
+
+export default Student
